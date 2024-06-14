7e3f0b3e6a35f0cb2e4f17e025ffdea41beeb97a
Binary Maestro <blackhorse00042@gmail.com>
2024-06-14 23:08:12 +0300

    feat: init

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  plugins: ["lodash", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:lodash/canonical", "plugin:@typescript-eslint/recommended"],
+  env: {
+    browser: false,
+    node: true
+  },
+  rules: {
+    camelcase: 0,
+    "object-curly-spacing": ["error", "always"],
+    "class-methods-use-this": 0,
+    "consistent-return": 0,
+    "default-case-last": 0,
+    "default-param-last": 0,
+    "function-paren-newline": 0,
+    "jsx-quotes": [2, "prefer-single"],
+    "no-param-reassign": 0,
+    "no-restricted-exports": 0,
+    "lodash/import-scope": 1,
+    "lodash/path-style": [2, "as-needed"],
+    "@typescript-eslint/ban-types": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-namespace": 0,
+    "no-restricted-imports": 0,
+    "no-shadow": 0,
+    "no-underscore-dangle": 0,
+    "no-unused-vars": 0,
+    "operator-linebreak": 0,
+    "prefer-destructuring": 2,
+    semi: ["error", "always"]
+  },
+  overrides: [
+    {
+      files: ["./src/**/*.*", "./framework/**/*.*"],
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    }
+  ]
+};
+
